960cee2a71987d6c2551083e49a7c58e74d6ea1e
Arno Noordover <anoordover@users.noreply.github.com>
2021-01-23 23:25:51 +0100

    Feature/session2 (#1)
    * first commit
    
    * End of session 2
    
    * Moved kebab to endpoint config

---
+++ source/Sample.Service/Program.cs
@@ -0,0 +1,37 @@
+using MassTransit;
+using MassTransit.Definition;
+using MassTransit.RabbitMqTransport;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Configuration;
+using Sample.Components.Consumers;
+
+namespace Sample.Service
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddMassTransit(cfg =>
+                    {
+                        cfg.AddConsumersFromNamespaceContaining<SubmitOrderConsumer>();
+                        cfg.UsingRabbitMq(ConfigureBus);
+                    });
+                    services.AddMassTransitHostedService();
+                });
+
+        private static void ConfigureBus(IBusRegistrationContext context,
+            IRabbitMqBusFactoryConfigurator configurator)
+        {
+            configurator.ConfigureEndpoints(context, KebabCaseEndpointNameFormatter.Instance);
+        }
+    }
+}
